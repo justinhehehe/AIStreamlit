f76ee1dcb93a0ad87dfe388d9c72dbefd61fa9c6
justinhehehe <justinwym-wm22@student.tarc.edu.my>
2024-09-12 15:19:07 +0800

    Update 🔎_Recommendation.py

--- 🔎_Recommendation.py
+++ 🔎_Recommendation.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 st.title("🎧Music Recommender🎧")
-with st.container():
+
+with st.container(border=None):
   st.write("Music is a universal language that transcends borders and connects people across cultures and generations. It has the power to evoke emotions, tell stories, and create memories through rhythm, melody, and harmony. Whether it's the upbeat energy of pop, the soothing tones of classical, or the raw emotion of blues, music resonates deeply with listeners, offering a form of expression and escape. It enriches our daily lives, brings people together in celebration, and often serves as a reflection of societal moments and personal experiences.")
 
 st.sidebar.success("Select type of recommender")
