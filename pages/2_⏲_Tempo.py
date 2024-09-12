009e24d5f2f56eaee8c06eb251590f1323bc6fec
justinhehehe <justinwym-wm22@student.tarc.edu.my>
2024-09-12 14:11:45 +0800

    Update 2_⏲_Tempo.py

--- pages/2_⏲_Tempo.py
+++ pages/2_⏲_Tempo.py
@@ -76,7 +76,7 @@ def recommend_song(song_name, artist_name):
         song, artist = rec
         if song != closest_song and (song, artist) not in recommended_songs:  # Avoid duplicates and the input song
             recommended_songs.add((song, artist))
-            st.write(f"'{song}' by {artist}")
+            st.write(f"'**{song}**' by **{artist}**")
 
 
 # Streamlit interface
